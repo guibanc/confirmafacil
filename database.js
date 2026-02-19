5c4e13481d247c38871f3bd4ef253593c1b5e0bb
Seu Nome <guibanc@yahoo.com>
2026-02-19 20:01:33 -0300

    fix(database): rebuild dependencies for railway

--- database.js
+++ database.js
@@ -1,30 +1,34 @@
 const Database = require("better-sqlite3");
 
+// abre ou cria o arquivo
 const db = new Database("database.db");
+
+// melhora segurança de FK
+db.pragma("foreign_keys = ON");
 
 // Criação das tabelas
 db.exec(`
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email TEXT UNIQUE,
-    password TEXT
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
   );
 
   CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    slug TEXT UNIQUE,
-    user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    name TEXT NOT NULL,
+    slug TEXT UNIQUE NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
   );
 
   CREATE TABLE IF NOT EXISTS guests (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+    name TEXT NOT NULL,
     email TEXT,
-    event_id INTEGER,
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    event_id INTEGER NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
   );
 `);
 
